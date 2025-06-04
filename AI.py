731a01a0fc41e02c76db4881155e44e7aff9f6c6
emlosoomro <soomroeman2008@gmail.com>
2025-06-04 12:28:20 +0500

    rulebasedchatbot

--- AI.py
+++ AI.py
@@ -1,1 +1,12 @@
 print("hello i am AI bot. what is your name?")
+name=input()
+print("nice to meet you",name)
+print("how are you feeling today. good or bad")
+mood=input().lower()
+if mood=="good":
+    print("i am glad to hear that")
+elif mood=="bad":
+    print("sorry to hear that")
+else:
+    print("i see sometimes its hard to put feelings in words")
+print("it was nice meeting you",name,"good bye")
